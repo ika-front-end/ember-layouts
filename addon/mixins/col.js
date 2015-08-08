60a8c484537d886aa1c2256a0165a593319b8f4a
kkdashu <wmeng17@gmail.com>
2015-08-08 21:37:36 +0800

    给col组件增加pull, push, offset属性

--- addon/mixins/col.js
+++ addon/mixins/col.js
@@ -1,10 +1,45 @@
 import Em from 'ember';
 
 export default Em.Mixin.create({
+  classNameBindings: ['colClass', 'offsetClass', 'pushClass', 'pullClass'],
+  /**
+   * 列值
+   * 默认为3
+   **/
   col: 3,
-  classNameBindings: ['colClass'],
-  colClass: Em.computed('col' ,function(){
+  colClass: Em.computed('col', function(){
     var col = this.get('col') || 4;
     return 'col-md-' + col;
+  }),
+
+  /**
+   * 偏移值
+   * 偏移多少列
+   **/
+  offset: 0,
+  offsetClass: Em.computed('offset', function() {
+    var offset = this.get('offset');
+    if(!!offset) {
+      return 'col-md-offset-' + offset;
+    }
+    return '';
+  }),
+
+  pull: 0,
+  pullClass: Em.computed('pull', function() {
+    var pull = this.get('pull');
+    if(!!pull) {
+      return 'col-md-pull-' + pull;
+    }
+    return '';
+  }),
+
+  push: 0,
+  pushClass: Em.computed('push', function() {
+    var push = this.get('push');
+    if(!!push) {
+      return 'col-md-push-' + push;
+    }
+    return '';
   })
 });
--- tests/unit/mixins/col-test.js
+++ tests/unit/mixins/col-test.js
@@ -10,11 +10,28 @@ module('Testing Col Mixin', {
   }
 });
 
+test('default col', function(assert) {
+  var c = Component.create();
+  Em.run(function(){
+    c.append();
+  });
+  assert.ok(c.$().hasClass('col-md-3'));
+  assert.ok(c.$().attr('class').indexOf('col-md-offset') === -1);
+  assert.ok(c.$().attr('class').indexOf('col-md-pull') === -1);
+  assert.ok(c.$().attr('class').indexOf('col-md-push') === -1);
+});
+
 test('set col', function(assert) {
   var c = Component.create();
   Em.run(function(){
     c.append();
     c.set('col', 2);
+    c.set('offset', 2);
+    c.set('push', 2);
+    c.set('pull', 2);
   });
   assert.ok(c.$().hasClass('col-md-2'));
+  assert.ok(c.$().hasClass('col-md-pull-2'));
+  assert.ok(c.$().hasClass('col-md-push-2'));
+  assert.ok(c.$().hasClass('col-md-offset-2'));
 });
