7dd4fc2c491530ac17c640c85c6d4924f7989764
kkdashu <wmeng17@gmail.com>
2015-08-08 18:52:03 +0800

    增加em-row组件

---
+++ addon/components/row.js
@@ -0,0 +1,4 @@
+import Em from 'ember';
+import RowMixin from 'ember-ika-layouts/mixins/row';
+
+export default Em.Component.extend(RowMixin, {});
---
+++ app/components/em-row.js
@@ -0,0 +1,4 @@
+import Em from 'ember';
+import EmRow from 'ember-ika-layouts/components/row';
+
+export default EmRow;
